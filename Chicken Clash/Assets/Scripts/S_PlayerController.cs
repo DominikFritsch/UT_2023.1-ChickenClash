12b3610ad8e8f5e6821cf633150840002fd885fe
Dominik Fritsch <104722098+DominikFritsch@users.noreply.github.com>
2023-01-06 11:53:05 +0100

    Update S_PlayerController.cs
    The script was cleaned up and the version of the project was set to 0.2.

--- Chicken Clash/Assets/Scripts/S_PlayerController.cs
+++ Chicken Clash/Assets/Scripts/S_PlayerController.cs
@@ -6,7 +6,7 @@ public class S_PlayerController : MonoBehaviour
 {
     /* PUBLIC VARIABLES */
     #region
-    public float force = 10.0f;
+    public float force = 15.0f;
     public float zBoundary = 10.6f;
     #endregion
 
@@ -18,17 +18,27 @@ public class S_PlayerController : MonoBehaviour
 
     /* METHODS */
     #region
-    void HorizontalMovement()
+    // Moves the player in all directions based on horizontal and vertical input
+    void MovePlayer()
     {
         var horizontalInput = UnityEngine.Input.GetAxis("Horizontal");
+        var verticalInput = UnityEngine.Input.GetAxis("Vertical");
+
+        // Horizontal movement
         var direction = UnityEngine.Vector3.right;
         playerRigidbody.AddForce(direction * horizontalInput * force);
-    }
-    void VerticalMovement()
-    {
-        var verticalInput = UnityEngine.Input.GetAxis("Vertical");
-        var direction = UnityEngine.Vector3.forward;
+
+        // Vertical movement
+        direction = UnityEngine.Vector3.forward;
         playerRigidbody.AddForce(direction * verticalInput * force);
+    }
+    // Prevent the player from leaving the top or bottom
+    void ConstrainPlayerPosition()
+    {
+        if (playerTransform.position.z >= zBoundary)
+            playerTransform.position = new UnityEngine.Vector3(playerTransform.position.x, playerTransform.position.y, zBoundary);
+        if (playerTransform.position.z <= -zBoundary)
+            playerTransform.position = new UnityEngine.Vector3(playerTransform.position.x, playerTransform.position.y, -zBoundary);
     }
     #endregion
 
@@ -45,17 +55,8 @@ public class S_PlayerController : MonoBehaviour
     }
     void FixedUpdate()
     {
-        if(playerTransform.position.z >= zBoundary)
-            playerTransform.position = new UnityEngine.Vector3(playerTransform.position.x, playerTransform.position.y, zBoundary);
-        else
-            VerticalMovement();
-
-        if (playerTransform.position.z <= -zBoundary)
-            playerTransform.position = new UnityEngine.Vector3(playerTransform.position.x, playerTransform.position.y, -zBoundary);
-        else
-            VerticalMovement();
-
-        HorizontalMovement();
+        MovePlayer();
+        ConstrainPlayerPosition();
     }
     #endregion
 
