573fba02ebdb25f5948f34a290a4348989a8ad6e
Dominik Fritsch <104722098+DominikFritsch@users.noreply.github.com>
2023-01-06 10:39:15 +0100

    Update S_PlayerController.cs
    Horizontal and vertical movements were added to the script.

--- Chicken Clash/Assets/Scripts/S_PlayerController.cs
+++ Chicken Clash/Assets/Scripts/S_PlayerController.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class S_PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    /* PUBLIC VARIABLES */
+    #region
+    public float force = 10.0f;
+    #endregion
+
+    /* PRIVATE VARIABLES */
+    #region
+    UnityEngine.Rigidbody playerRigidbody;
+    #endregion
+
+    /* METHODS */
+    #region
+    void HorizontalMovement()
+    {
+        var horizontalInput = UnityEngine.Input.GetAxis("Horizontal");
+        var direction = UnityEngine.Vector3.right;
+        playerRigidbody.AddForce(direction * horizontalInput * force);
+    }
+    void VerticalMovement()
+    {
+        var verticalInput = UnityEngine.Input.GetAxis("Vertical");
+        var direction = UnityEngine.Vector3.forward;
+        playerRigidbody.AddForce(direction * verticalInput * force);
+    }
+    #endregion
+
+    /* STANDARD METHODS */
+    #region
     void Start()
     {
-        
+        playerRigidbody = gameObject.GetComponent<UnityEngine.Rigidbody>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        HorizontalMovement();
+        VerticalMovement();
     }
+    #endregion
+
 }
